4f9730964b970ac1dce01d1d6c0b31fa0ea5db20
kote spirandi <konstantine.spirandi@gmail.com>
2023-08-06 22:57:18 +0400

    uibuild

--- index.js
+++ index.js
@@ -16,31 +16,9 @@ app.use(
 );
 app.use(cors());
 
-app.use(express.json());
-
 app.use(express.static("build"));
-let persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
+
+app.use(express.json());
 
 const date = new Date();
 
@@ -50,7 +28,7 @@ app.get("/", (request, response) => {
 
 app.get("/info", (request, response) => {
   response.send(
-    `<p>Phonebook has info for ${persons.length} people</p><br/>${date}`
+    `<p>Phonebook has info for ${"who knows how many"} people</p><br/>${date}`
   );
 });
 
@@ -60,56 +38,52 @@ app.get("/api/persons", (request, response) => {
   });
 });
 
-app.get("/api/persons/:id", (request, response) => {
-  Person.findById(request.params.id).then((person) => {
-    response.json(person);
-  });
-
-  // const id = Number(request.params.id);
-  // const person = persons.find((person) => person.id === id);
-
-  // if (person) {
-  //   response.json(person);
-  // } else {
-  //   response.status(404).end();
-  // }
-  // console.log(person);
-  // response.json(person);
+app.get("/api/persons/:id", (request, response, next) => {
+  Person.findById(request.params.id)
+    .then((person) => {
+      if (person) {
+        response.json(person);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
 });
 
-app.delete("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  persons = persons.filter((person) => person.id !== id);
-
-  response.status(204).end();
+app.delete("/api/persons/:id", (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
 });
-
-// const generateId = () => {
-//   const maxId = persons.length > 0 ? Math.max(...persons.map((n) => n.id)) : 0;
-//   return maxId + 1;
-// };
 
 app.post("/api/persons", (request, response) => {
   const body = request.body;
-  console.log(body);
+  if (body.name.length == 0 || body.number.length == 0) {
+    return response.status(400).json({ error: "content missing" });
+  } else {
+    const person = new Person({
+      name: body.name,
+      number: body.number,
+    });
+    person.save().then((savedContact) => {
+      response.json(savedContact);
+    });
+  }
+});
 
-  // if (!body.name || !body.number) {
-  //   return response.status(400).json({
-  //     error: "content missing",
-  //   });
-  // }
-  // if (persons.find((person) => person.name === body.name)) {
-  //   return response.status(403).json({
-  //     error: "name must be unique",
-  //   });
-  // }
-  const person = new Person({
+app.put("/api/persons/:id", (request, response, next) => {
+  const body = request.body;
+  const person = {
     name: body.name,
     number: body.number,
-  });
-  person.save().then((savedContact) => {
-    response.json(savedContact);
-  });
+  };
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then((updatedPerson) => {
+      response.json(updatedPerson);
+    })
+    .catch((error) => next(error));
 });
 
 const unknownEndpoint = (request, response) => {
@@ -118,7 +92,21 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint);
 
-const PORT = process.env.PORT || 3001;
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  } else if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message });
+  }
+
+  next(error);
+};
+
+app.use(errorHandler);
+
+const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
