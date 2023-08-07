e524485aabd8c8add389fa1fa1115b35a6ee495c
kote spirandi <konstantine.spirandi@gmail.com>
2023-08-07 16:57:58 +0400

    uibuild

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ app.delete("/api/persons/:id", (request, response, next) => {
     .catch((error) => next(error));
 });
 
-app.post("/api/persons", (request, response) => {
+app.post("/api/persons", (request, response, next) => {
   const body = request.body;
   if (body.name.length == 0 || body.number.length == 0) {
     return response.status(400).json({ error: "content missing" });
@@ -67,19 +67,26 @@ app.post("/api/persons", (request, response) => {
       name: body.name,
       number: body.number,
     });
-    person.save().then((savedContact) => {
-      response.json(savedContact);
-    });
+    person
+      .save()
+      .then((savedContact) => {
+        response.json(savedContact);
+      })
+      .catch((error) => next(error));
   }
 });
 
 app.put("/api/persons/:id", (request, response, next) => {
-  const body = request.body;
+  const { name, number } = request.body;
   const person = {
-    name: body.name,
-    number: body.number,
+    name: name,
+    number: number,
   };
-  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+  Person.findByIdAndUpdate(
+    request.params.id,
+    { name, number },
+    { new: true, runValidators: true, context: "query" }
+  )
     .then((updatedPerson) => {
       response.json(updatedPerson);
     })
@@ -93,8 +100,6 @@ const unknownEndpoint = (request, response) => {
 app.use(unknownEndpoint);
 
 const errorHandler = (error, request, response, next) => {
-  console.error(error.message);
-
   if (error.name === "CastError") {
     return response.status(400).send({ error: "malformatted id" });
   } else if (error.name === "ValidationError") {
--- models/person.js
+++ models/person.js
@@ -16,8 +16,23 @@ mongoose
   });
 
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
+  name: {
+    type: String,
+    minlength: 3,
+    required: [true, "user name is required"],
+  },
+  number: {
+    type: String,
+    minlength: 8,
+    validate: {
+      validator: function (v) {
+        return /\d{3}-\d{3}-\d{4}/.test(v);
+      },
+      message: (props) =>
+        `${props.value} is not a valid phone number! example: 333-333-4444`,
+    },
+    required: [true, "user phone number required"],
+  },
 });
 
 personSchema.set("toJSON", {
